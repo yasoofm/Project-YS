efd9447dce9fdca76cd3f0365b5de7f33bc3b68e
Yousef T Y Y A Mandani <83332@kfh.com>
2024-01-10 10:07:10 +0300

    add create account

--- Bank_Account_Class_task/src/App.java
+++ Bank_Account_Class_task/src/App.java
@@ -1,8 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        BankAccount bankAccount = new BankAccount(1234567898, 700.45);
+        BankAccount bankAccount = new BankAccount(700.45);
         bankAccount.deposit(50);
         bankAccount.withdrawal(33.5);
         bankAccount.displayingTransactionsHistory();
+
+        User yousef = new User(null, null);
+        User saleh = new User(null, null);
+
+        yousef.createAccount();
+        saleh.createAccount();
+        System.out.println(yousef.getBankAccount());
+        System.out.println(saleh.getBankAccount());
     }
 }
