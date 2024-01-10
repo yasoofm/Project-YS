9cf68c8ee6821c77faa86817ff034a4d17dd6835
Saleh-Binessa <83325@kfh.com>
2024-01-10 09:54:18 +0300

    Adding Bank Singleton

--- Bank_Account_Class_task/src/BankSingleton.java
+++ Bank_Account_Class_task/src/BankSingleton.java
@@ -1,5 +1,26 @@
 import java.util.ArrayList;
 
 public class BankSingleton {
-    private ArrayList<BankAccount> accounts;
+    private static ArrayList<BankAccount> accounts;
+    private static BankSingleton instance;
+
+    public static BankSingleton getInstance() {
+        if (instance == null) {
+            instance = new BankSingleton();
+            accounts = new ArrayList<BankAccount>();
+        }
+        return instance;
+    }
+
+    public void storeAccount(BankAccount account) {
+        // Code to store account
+        accounts.add(account);
+
+    }
+
+    public ArrayList<BankAccount> retrieveAccounts() {
+        // Code to retrieve account
+        return accounts;
+    }
+
 }
